2892ef37ef34f476bdb8953f555a5fcaca7ee365
Muhammad Hadi <hadiquershi08@gmail.com>
2026-02-04 18:01:35 +0500

    reinstalling docker

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -2,17 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./assest/css/bootstrap.min.css"
-import "./assest/css/style.css";
-import "./assest/css/owl.carousel.min.css"
-import "./assest/css/tempusdominus-bootstrap-4.min.css"
-
-// Global CSS (Vite bundles it)
-
-
-// Optional: import JS modules if you install via npm
-// import 'chart.js';
-// import 'owl.carousel';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
