eed034b1d54967b8991cc46463c251819a6e29ba
Muhammad Hadi <hadiquershi08@gmail.com>
2026-02-06 09:25:48 +0500

    CI/CD piline #3

--- Frontend/src/pages/Checkout.jsx
+++ Frontend/src/pages/Checkout.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Breadcrumb from '../components/Detail-page-component/BreadCrum'
+import Breadcrumb from '../components/Detail-page-component/BreadCrum.jsx'
 import { Link } from 'react-router';
 import { useCart } from '../Custom-context/CartProvider';
 import { useAuth } from '../Custom-context/AuthProvider';
